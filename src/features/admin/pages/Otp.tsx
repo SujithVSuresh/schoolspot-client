69593757b60590f1a8bf519ccd370a3bcc44f6fc
SujithVSuresh <sujitheternal13@gmail.com>
2025-02-20 11:14:55 +0530

    refactor: removed unwanted imports

--- src/features/admin/pages/Otp.tsx
+++ src/features/admin/pages/Otp.tsx
@@ -1,10 +1,15 @@
-import React, {useState, useRef} from 'react'
+import React, {useState, useRef, useEffect} from 'react'
 import Header from '../components/Header';
 import { useNavigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import { verify, resendOtp } from '../api/api';
 
 
 const Otp = () => {
     const navigate = useNavigate()
+
+    const location = useLocation()
+
 
       const [otp1, setOtp1] = useState("");
       const [otp2, setOtp2] = useState("");
@@ -12,9 +17,36 @@ const Otp = () => {
       const [otp4, setOtp4] = useState("");
       const [otp5, setOtp5] = useState("");
       const [otp6, setOtp6] = useState("");
-    
+
       const inputsRef = useRef<(HTMLInputElement | null)[]>([]);
-    
+
+      
+    const [timer, setTimer] = useState(60)
+
+    useEffect(() => {
+      const value = Number(localStorage.getItem("timer"))
+      if(value){
+        setTimer(value)
+      }
+
+      const interval = setInterval(() => {
+        setTimer(prevTime => {
+          if(prevTime > 0){
+            const time = prevTime-1
+            localStorage.setItem("timer", time.toString())
+            return time
+          }else{
+            return 0
+          }
+        })
+    }, 1000)
+
+    return () => {
+      clearInterval(interval);
+    };
+
+    }, [])
+        
       const handleChange = (index: number, value: string) => {
         if (!/^\d?$/.test(value)) return;
     
@@ -52,18 +84,37 @@ const Otp = () => {
         }
       };
     
-      const handleSubmit = (e: React.FormEvent) => {
+      const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
         const otpCode = otp1 + otp2 + otp3 + otp4 + otp5 + otp6;
         if (otpCode.length === 6) {
+          const response = await verify({
+            otp: Number(otpCode),
+            email: location.state.email
+          })
+          
+          if(response.success){
             navigate('/school-info')
+          }
+            
           console.log("OTP Submitted:", otpCode);
         } else {
           alert("Please enter all 6 digits.");
         }
       };
+
+
+      const handleResendSubmit = async (e: React.FormEvent) => {
+        e.preventDefault()
+        const response = await resendOtp({email: location.state.email})
+
+        if(response.success){
+          setTimer(60)
+        }
+
+      }
   return (
-    <>
+     <>
     <Header />
 
     <main className="h-screen flex justify-center items-center">
@@ -71,7 +122,7 @@ const Otp = () => {
       <h1 className="font-bold text-center text-4xl">Check your email</h1>
 
       <h5 className="text-base mt-5 mb-6 text-center">
-        We've sent your verification code to sujithvs131313@gmail.com
+        We've sent your verification code to {location?.state?.email}
       </h5>
 
       <div>
@@ -96,19 +147,28 @@ const Otp = () => {
           ))}
         </div>
 
-        <div
+        <button
           onClick={handleSubmit}
-          className={`bg-blue-700 w-full h-12 rounded-sm flex justify-center mt-10 items-center ${
-            otp1 && otp2 && otp3 && otp4 && otp5 && otp6 ? "cursor-pointer" : "cursor-not-allowed"
+          disabled={otp1 && otp2 && otp3 && otp4 && otp5 && otp6 ? false : true}
+          className={`w-full h-12 rounded-sm flex justify-center mt-10 items-center ${
+            otp1 && otp2 && otp3 && otp4 && otp5 && otp6 ? "cursor-pointer bg-blue-700" : "cursor-not-allowed bg-blue-200"
           }`}
         >
           <h1 className="text-base font-medium text-white">Next</h1>
-        </div>
-
-        <h5 className="font-medium mt-5">
-          Didn't get the email?{" "}
-          <span className="text-blue-500 cursor-pointer font-semibold">Resend</span>
+        </button>
+        
+        {timer<=0 ? (
+            <h5 className="font-medium mt-5">
+             Didn't get the email?{" "}
+            <span className="text-blue-500 cursor-pointer font-semibold" onClick={(e) => handleResendSubmit(e)}>Resend</span>
+            </h5>
+        ) : (
+          <h5 className="font-normal mt-5">
+          Resend OTP in <span className='font-medium'>{timer} sec</span>
         </h5>
+
+        )}
+
       </div>
     </div>
     </main>
