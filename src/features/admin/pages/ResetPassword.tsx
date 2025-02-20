d6f99104a3020f5b696219cd3de984e2b7f686f5
SujithVSuresh <sujitheternal13@gmail.com>
2025-02-20 11:16:19 +0530

    feat: done the reset req and pw change page

--- src/features/admin/pages/ResetPassword.tsx
+++ src/features/admin/pages/ResetPassword.tsx
@@ -1,13 +1,56 @@
 import logo from "../../../assets/images/logo.png";
+import { useState } from "react";
+import { emailRegex } from "../../../app/validation/regex";
+import { Link } from "react-router-dom";
+import { passwordResetRequest } from "../api/api";
 
 
 const ResetPassword = () => {
+  const [email, setEmail] = useState("")
+
+  const [error, setError] = useState<{email: string}>({
+      email: ""
+    });
+
+
+  let isValid = true
+
+  const validate = () => {
+    const error: {email: string} = {
+      email: "",
+    }
+
+    if(!email){
+      error.email = "This field is required"
+      isValid = false
+    }else if(!emailRegex.test(email)){
+      error.email = "Enter a valid email"
+      isValid = false
+    }
+
+    return error
+  }
+
+
+  const handleFormSubmit = async (e: React.FormEvent) => {
+    e.preventDefault()
+
+    setError(validate())
+
+    if(isValid){
+      const response = await passwordResetRequest({email}) 
+
+      if(response.data){
+        console.log("password reset link send successfully", response.data)
+        setEmail("")
+      }
+    }
+  }
+
   return (
     <>
     <header className="h-20 flex items-center justify-between p-10">
       <img src={logo} alt="" className="h-10" />
-
-
     </header>
     <main className="h-screen flex justify-center items-center">
     <div className="w-[420px]">
@@ -27,19 +70,22 @@ const ResetPassword = () => {
       <input
         type="email"
         id="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
         className="w-full py-2 border-b-2 focus:ring-0 border-b-black outline-none"
       />
+      <p className='text-red-500 text-xs mt-1'>{error.email}</p>
 
-      <div className="bg-blue-700 h-12 rounded-sm flex justify-center mt-6 items-center">
+      <button onClick={(e) => handleFormSubmit(e)} disabled={email ? false : true} className={`${email ? "bg-blue-700" : "bg-blue-200"} h-12 w-full rounded-sm flex justify-center mt-6 items-center`}>
         <h1 className="text-base font-medium text-white">Reset password</h1>
-      </div>
+      </button>
     </div>
 
     <h5 className="font-medium text-center mt-5">
           Go back to{" "}
-          <span className="text-blue-500 cursor-pointer font-semibold">
+          <Link to={'/signin'} className="text-blue-500 cursor-pointer font-semibold">
             Sign in
-          </span>
+          </Link>
         </h5>
 
 
--- src/features/admin/pages/ResetPasswordForm.tsx
+++ src/features/admin/pages/ResetPasswordForm.tsx
@@ -1,13 +1,129 @@
+import logo from "../../../assets/images/logo.png";
+import React, { useState } from "react";
+import { passwordRegex } from "../../../app/validation/regex";
+import { useSearchParams } from "react-router-dom";
+import { passwordReset } from "../api/api";
 
+const ResetPasswordForm = () => {
+  const [searchParams] = useSearchParams();
+  const token = searchParams.get("token");
 
-// import React from 'react'
+  if (!token) {
+    console.log("no token provided");
+  }
 
-// const ResetPasswordForm = () => {
-//   return (
-//     <div>
-      
-//     </div>
-//   )
-// }
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
-// export default ResetPasswordForm
+  const [error, setError] = useState<{
+    password: string;
+    confirmPassword: string;
+  }>({
+    password: "",
+    confirmPassword: "",
+  });
+
+  let isValid = true;
+
+  const validate = () => {
+    const error: { password: string; confirmPassword: string } = {
+      password: "",
+      confirmPassword: "",
+    };
+
+    if (!password) {
+      error.password = "This field is required";
+      isValid = false;
+    } else if (!passwordRegex.test(password)) {
+      error.password = "Password should be atleast 8 character long";
+      isValid = false;
+    }
+
+    if (!confirmPassword) {
+      error.confirmPassword = "This field is required";
+      isValid = false;
+    } else if (confirmPassword != password) {
+      error.confirmPassword = "Password doesn't match";
+      isValid = false;
+    }
+    return error;
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    setError(validate());
+
+    if (isValid && token) {
+      const response = await passwordReset({ token, password });
+      console.log("submit the reset form", response);
+    }
+  };
+
+  return (
+    <>
+      <header className="h-20 flex items-center justify-between p-10">
+        <img src={logo} alt="" className="h-10" />
+      </header>
+      <main className="h-screen flex justify-center items-center">
+        <div className="w-[420px]">
+          <h1 className="font-bold text-center text-4xl">Enter new password</h1>
+
+          <h5 className="text-base text-center mt-5 mb-6">
+            Please enter your new password to reset your old one.
+          </h5>
+
+          <div className="mb-5">
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Password
+            </label>
+            <div className="relative">
+              <input
+                type="password"
+                id="password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                className="w-full py-2 border-b-2 focus:ring-0 border-b-black outline-none"
+              />
+            </div>
+            <p className="text-red-500 text-xs mt-1">{error.password}</p>
+          </div>
+
+          <div className="mb-5">
+            <label
+              htmlFor="confirmPassword"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Confirm Password
+            </label>
+            <div className="relative">
+              <input
+                type="password"
+                id="confirmPassword"
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                value={confirmPassword}
+                className="w-full py-2 border-b-2 focus:ring-0 border-b-black outline-none"
+              />
+            </div>
+            <p className="text-red-500 text-xs mt-1">{error.confirmPassword}</p>
+          </div>
+
+          <button
+            disabled={password && confirmPassword ? false : true}
+            onClick={(e) => handleSubmit(e)}
+            className={`w-full mt-5 h-12 rounded-sm flex justify-center items-center ${
+              password && confirmPassword ? "bg-blue-700" : "bg-blue-200"
+            }`}
+          >
+            <h1 className="text-base font-medium text-white">Submit</h1>
+          </button>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default ResetPasswordForm;
