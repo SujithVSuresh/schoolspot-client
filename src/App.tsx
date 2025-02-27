fdf52a39649e7fd062ebfb245160b3711ef7ac9e
SujithVSuresh <sujitheternal13@gmail.com>
2025-02-27 11:59:21 +0530

    refactor: abstract admin router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Signup from './features/admin/pages/Signup';
-import Signin from './features/admin/pages/Signin';
-import ResetPassword from './features/admin/pages/ResetPassword';
-import ResetPasswordForm from './features/admin/pages/ResetPasswordForm';
-import Otp from './features/admin/pages/Otp';
-import Student from './features/admin/pages/Student';
-import ProtectedRoute from './features/admin/routes/ProtectedRoute';
-import Teachers from './features/admin/pages/Teachers';
-import AdminProfile from './features/admin/pages/AdminProfile';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import { ToastContainer } from 'react-toastify';
+import AdminRoute from './features/admin/routes/AdminRoute';
 
 
 function App() {
@@ -16,19 +9,8 @@ function App() {
     <>
      <ToastContainer />
     <Router>
-      <Routes>
-        <Route path="/signup" element={<ProtectedRoute isLogin={false}><Signup /></ProtectedRoute>} />
-        <Route path="/signin" element={<ProtectedRoute isLogin={false}><Signin /></ProtectedRoute>} />
-        <Route path="/signin/forgot" element={<ProtectedRoute isLogin={false}><ResetPassword /></ProtectedRoute>} />
-        <Route path="/signin/reset-password" element={<ProtectedRoute isLogin={false}><ResetPasswordForm /></ProtectedRoute>} />
-        {/* <Route path="/pricing" element={<ProtectedRoute isLogin={true}></ProtectedRoute><Pricing />} /> */}
-        {/* <Route path="/school-info" element={<ProtectedRoute isLogin={true}></ProtectedRoute><SchoolInfoForm />} /> */}
-        <Route path="/otp" element={<ProtectedRoute isLogin={false}><Otp /></ProtectedRoute>} />
-        <Route path="/profile" element={<ProtectedRoute isLogin={true}><AdminProfile /></ProtectedRoute>} />
+        <AdminRoute />
 
-        <Route path="/students" element={<ProtectedRoute isLogin={true}><Student /></ProtectedRoute>} />
-        <Route path="/teachers" element={<ProtectedRoute isLogin={true}><Teachers /></ProtectedRoute>} />
-      </Routes>
     </Router>
     </>
   )
---
+++ src/features/admin/routes/AdminRoute.tsx
@@ -0,0 +1,101 @@
+import { Route, Routes } from "react-router-dom";
+import ProtectedRoute from "./ProtectedRoute";
+import Signup from "../pages/Signup";
+import Signin from "../pages/Signin";
+import ResetPassword from "../pages/ResetPassword";
+import ResetPasswordForm from "../pages/ResetPasswordForm";
+import Otp from "../pages/Otp";
+import AdminProfile from "../pages/AdminProfile";
+import Student from "../pages/Student";
+import Teachers from "../pages/Teachers";
+import Pricing from "../pages/Pricing";
+import SchoolInfoForm from "../pages/SchoolInfoForm";
+
+const AdminRoute = () => {
+  return (
+    <Routes>
+      <Route
+        path="/signup"
+        element={
+          <ProtectedRoute isLogin={false}>
+            <Signup />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/signin"
+        element={
+          <ProtectedRoute isLogin={false}>
+            <Signin />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/signin/forgot"
+        element={
+          <ProtectedRoute isLogin={false}>
+            <ResetPassword />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/signin/reset-password"
+        element={
+          <ProtectedRoute isLogin={false}>
+            <ResetPasswordForm />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/pricing"
+        element={
+          <ProtectedRoute isLogin={true}>
+            <Pricing />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/school-info"
+        element={
+          <ProtectedRoute isLogin={true}>
+            <SchoolInfoForm />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/otp"
+        element={
+          <ProtectedRoute isLogin={false}>
+            <Otp />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/profile"
+        element={
+          <ProtectedRoute isLogin={true}>
+            <AdminProfile />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/students"
+        element={
+          <ProtectedRoute isLogin={true}>
+            <Student />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/teachers"
+        element={
+          <ProtectedRoute isLogin={true}>
+            <Teachers />
+          </ProtectedRoute>
+        }
+      />
+    </Routes>
+  );
+};
+
+export default AdminRoute;
