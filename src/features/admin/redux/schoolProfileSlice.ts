327c856e44dcae86e85c169e89962da7c75c47bd
SujithVSuresh <sujitheternal13@gmail.com>
2025-03-01 20:51:14 +0530

    feat: created the school profile slice

---
+++ src/features/admin/redux/schoolProfileSlice.ts
@@ -0,0 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { SchoolProfileType } from "../types/types";
+
+const initialState: SchoolProfileType = {
+    schoolName: '',
+    email: '',
+    phoneNumber: '',
+    principalName: '',
+    yearEstablished: 0,
+    regNumber: '',
+    totalStudents: 0,
+    totalTeachers: 0,
+    board: '',
+    city: '',
+    country: '',
+    state: '',
+    postalCode: '',
+    websiteUrl: ''
+}
+
+const schoolProfileSlice = createSlice({
+    name: 'schoolProfile',
+    initialState,
+    reducers: {
+        setSchoolProfile: (state, action) => {
+            return {...state, ...action.payload}
+        }
+    }
+})
+
+export const {setSchoolProfile} = schoolProfileSlice.actions;
+export const schoolProfileReducer = schoolProfileSlice.reducer;
+
